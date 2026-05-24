44d004a5760eb6cd1da9efc01880f2460d5165bc
JorgeSabattin <165532924+JorgeSabattin@users.noreply.github.com>
2026-05-24 17:08:28 -0400

    Update build.sh

--- heatmap_project/build.sh
+++ heatmap_project/build.sh
@@ -2,6 +2,7 @@
 set -o errexit
 pip install --upgrade pip setuptools wheel
 pip install pandas==3.0.3 --only-binary=:all:
+pip install "openpyxl>=3.1.5" --only-binary=:all:
 pip install -r requirements.txt --no-deps
 pip install Django==4.2.13 gunicorn==21.2.0 openpyxl==3.1.2 xlsxwriter==3.1.9 whitenoise==6.7.0
 python manage.py collectstatic --no-input
